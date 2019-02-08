2e41b8076fbefeb9fe3ae055d5642b00488c70c6
Hayden Shively <haydenshively@gmail.com>
2019-02-08 11:54:15 -0800

    Completed very basic structure

---
+++ Coury Leadership Program/Coury Leadership Program/Controllers/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Coury Leadership Program
+//
+//  Created by Hayden Shively on 2/8/19.
+//  Copyright © 2019 USC Marshall School of Business. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
